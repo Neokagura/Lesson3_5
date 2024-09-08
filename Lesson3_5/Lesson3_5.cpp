9849ca05852ba12a5e8d8db8ecd090cb550c6d06
Ness <bur>
2024-09-08 17:07:50 +0300

    Добавьте файлы проекта.

---
+++ Lesson3_5/Lesson3_5.cpp
@@ -0,0 +1,21 @@
+﻿
+#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int firstNum;
+	int secondNum;
+	cout << "Введите первое число: ";
+	cin >> firstNum;
+	cout << "Введите второе число: ";
+	cin >> secondNum;
+	cout << "-----Проверяем-----\n";
+	if (firstNum % secondNum == 0) {
+		cout << "Да, " << firstNum << " делится на " << secondNum << " без остатка!\n";
+	}
+	else {
+		cout << "Нет, " << firstNum << " не делится на " << secondNum << " без остатка!";
+	}
+
+}
